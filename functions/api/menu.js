95e2ad7a6a1d509448a427c6709ae62c0b278b58
plascore <wasupmyguy111@gmail.com>
2025-11-11 11:28:04 -0500

    changed json

--- functions/api/menu.js
+++ functions/api/menu.js
@@ -1,13 +1,28 @@
-export async function onRequestPost(context) {
-  const body = await context.request.text();
-
+export async function onRequest(context) {
   const response = await fetch("https://api.schoolnutritionandfitness.com/graphql", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body,
+    body: JSON.stringify({
+      operationName: "GetMenu",
+      variables: {
+        menuTypeId: "599742bd4d4a13a5438b4567",
+        monthId: "68890313615e6713f665ab47"
+      },
+      query: `
+        query GetMenu($menuTypeId: ID!, $monthId: ID!) {
+          menuType(id: $menuTypeId) {
+            items(monthId: $monthId) {
+              date
+              product { name prod_calories }
+            }
+          }
+        }
+      `
+    }),
   });
 
-  return new Response(await response.text(), {
+  const data = await response.text();
+  return new Response(data, {
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
